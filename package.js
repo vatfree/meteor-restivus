53b5dff90e411785d6b3f9e36e113e39c9a7ef46
Jaco Swarts <jaco@vatfree.com>
2023-04-01 17:03:44 +0200

    Update supported meteor version

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'vatfree:restivus',
   summary: 'Create authenticated REST APIs in Meteor via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
-  version: '1.1.2',
+  version: '1.2.0',
   git: 'https://github.com/vatfree/meteor-restivus.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(['1.10', '2.0']);
+  api.versionsFrom(['2.7']);
 
   // Meteor dependencies
   api.use('ecmascript');
   api.use('check');
   api.use('underscore');
-  api.use('accounts-password@~2.2.0');
+  api.use('accounts-password');
   api.use('simple:json-routes@2.1.0');
   api.use('alanning:roles@1.3.0', 'server', {weak: true});
 
